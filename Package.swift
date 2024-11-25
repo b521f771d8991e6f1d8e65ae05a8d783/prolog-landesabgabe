77bb48ff0bd5f4d77ade13fb6b548bb89ba514ca
Alex <kiwi-uncurled-salt@duck.com>
2024-11-25 14:11:48 +0100

    added simple REST server to handle the requests

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "LX",
+    products: [
+        .executable(
+            name: "LX",
+            targets: ["LX"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor", from: "4.106.4")
+    ],
+    targets: [
+        .executableTarget(
+            name: "LX",
+            dependencies: [
+                .product(name: "Vapor", package: "Vapor")
+            ],
+            exclude: ["CMakeLists.txt"])
+    ]
+)
--- Sources/LX/main.swift
+++ Sources/LX/main.swift
@@ -1,11 +1,39 @@
-import ActKit
-import BuildInformation
+//import ActKit
+//import BuildInformation
 import Foundation
-import LogicKit
+import Vapor
 
-//import Vapor
+//import LogicKit
 
-print(
-    "Running digital law server in version: ",
-    String(nobs.BuildInformation.getCurrentVersionAsString())
-)
+//let version = String(nobs.BuildInformation.getCurrentVersionAsString())
+let version = "1.0"
+
+print("Running digital law server in version: \(version)")
+
+// configures your application
+public func configure(_ app: Application) throws {
+    try routes(app)
+}
+
+func routes(_ app: Application) throws {
+    app.get("status") { req async -> String in
+        "OK"
+    }
+
+    app.get("version") { req async -> String in
+        return version
+    }
+
+    app.get { req async throws -> String in
+        guard let query: String = req.query[String.self, at: "query"] else {
+            throw Abort(.badRequest)
+        }
+        let result: String = "OK \(query)"
+        return result
+    }
+}
+
+let app = Application()
+defer { app.shutdown() }
+try configure(app)
+try app.run()

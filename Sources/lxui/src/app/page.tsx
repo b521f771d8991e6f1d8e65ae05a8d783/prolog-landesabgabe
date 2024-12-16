81dabc488c3a91ea6a8d9cc3355fbeca4a84dc6f
Alex <kiwi-uncurled-salt@duck.com>
2024-12-16 07:08:39 +0100

    added simple Prolog serialization class

--- Sources/lxui/src/app/page.tsx
+++ Sources/lxui/src/app/page.tsx
@@ -10,20 +10,41 @@ function Form() {
   return <></>
 }
 
-`
-subjekt(sachverhalt, max_mustermann).
-vorname(max_mustermann, "Max").
-nachname(max_mustermann, "Mustermann").
-natuerliche_person(max_mustermann).
-berufsmaessig(max_mustermann).
-
-verbum(sachverhalt, max_mustermann, bergbau(gewinnen, obertags, mineralische_rohstoffe)).
-objekt(sachverhalt, max_mustermann, bergbau(gewinnen, obertags, mineralische_rohstoffe), mein_gestein).
-gefoerdert(mein_gestein, 2, tonne).
-verwertet_am(mein_gestein, date(2024, 11, 12, 0, 0, 0, Off, TZ, DST)).`;
-
 interface PrologFragment {
-  serialize(): string;
+  serialize2Prolog(): string;
+}
+
+function generateUUID(id: string) {
+  return `${id}_${uuidv4().replaceAll("-", "")}`;
+}
+
+const GESTEIN_ID = "gestein";
+const PERSON_ID = "person";
+
+class LandesabgabeHandlung implements PrologFragment {
+  person_id: string;
+  sachverhalt_id: string;
+  gefördert?: number;
+  date: Date;
+  einheit: string = "tonne";
+
+  constructor(sachverhalt_id: string, person_id: string, date: Date, gefördert?: number) {
+    this.sachverhalt_id = sachverhalt_id;
+    this.person_id = person_id;
+    this.gefördert = gefördert;
+    this.date = date;
+  }
+
+  serialize2Prolog(): string {
+    const uuidWithPrefix = generateUUID(GESTEIN_ID);
+
+    return `
+      verbum(${this.sachverhalt_id}, ${this.person_id}, bergbau(gewinnen, obertags, mineralische_rohstoffe)).
+      objekt(this.sachverhalt_id, ${this.person_id}, bergbau(gewinnen, obertags, mineralische_rohstoffe), ${uuidWithPrefix}).
+      ${this.gefördert ? `gefoerdert(${uuidWithPrefix}, ${this.gefördert!}, ${this.einheit}).` : ""}
+      verwertet_am(${uuidWithPrefix}, date(${this.date.getFullYear()}, ${this.date.getMonth()}, ${this.date.getDay()}, 0, 0, 0, Off, TZ, DST)).
+    `; // TODO Stunde und Minute übernehmen
+  }
 }
 
 class LandesabgabePerson implements PrologFragment {
@@ -31,31 +52,33 @@ class LandesabgabePerson implements PrologFragment {
   vorname: string;
   nachname: string;
   alter: number;
+  berufsmäßig: boolean;
 
-  constructor(sachverhalt: string, vorname: string, nachname: string, alter: number) {
+  constructor(sachverhalt: string, vorname: string, nachname: string, alter: number, berufsmäßig: boolean = true) {
     this.sachverhalt = sachverhalt;
     this.vorname = vorname;
     this.nachname = nachname;
     this.alter = alter;
+    this.berufsmäßig = berufsmäßig;
   }
 
-  serialize(): string {
-    const uuid: string = uuidv4();
-    const uuidWithPrefx = `sachverhalt_${uuid.replaceAll("-", "")}`;
+  serialize2Prolog(): string {
+    const uuidWithPrefix = generateUUID(PERSON_ID);
 
     return `
-      subjekt(${this.sachverhalt}, ${uuidWithPrefx}).
-      vorname(${uuidWithPrefx}, "${this.vorname}").
-      nachname(${uuidWithPrefx}, "${this.nachname}").
-      natuerliche_person(${uuidWithPrefx}).
-      alter(${uuidWithPrefx}, ${this.alter}).
+      subjekt(${this.sachverhalt}, ${uuidWithPrefix}).
+      vorname(${uuidWithPrefix}, "${this.vorname}").
+      nachname(${uuidWithPrefix}, "${this.nachname}").
+      natuerliche_person(${uuidWithPrefix}).
+      alter(${uuidWithPrefix}, ${this.alter}).
+      ${this.berufsmäßig ? "" : `berufsmaessig(${uuidWithPrefix}).`}
     `;
   }
 }
 
 export default function Home() {
   let i = new LandesabgabePerson("sachverhalt", "vorname", "nachname", 23);
-  console.log(i.serialize());
+  console.log(i.serialize2Prolog());
   return <>
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <Form>

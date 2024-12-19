dea6186fda97458c99fa2118c57b5b3d1e21c32c
Alex <kiwi-uncurled-salt@duck.com>
2024-12-19 10:32:10 +0100

    simplified flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "LX";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/8b27c1239e5c421a2bbc2c65d52e4a6fbf2ff296";
     flake-utils.url = "github:numtide/flake-utils/04c1b180862888302ddfb2e3ad9eaa63afc60cf8";
@@ -10,11 +8,6 @@
   flake-utils.lib.eachSystem [
     # supported linux systems
     flake-utils.lib.system.x86_64-linux
-    flake-utils.lib.system.aarch64-linux
-
-    # supported darwin systems
-    #flake-utils.lib.system.x86_64-darwin
-    #flake-utils.lib.system.aarch64-darwin
   ] ( system:
     let
       pkgs = import nixpkgs {
@@ -22,42 +15,36 @@
         config.allowUnfree = false;
       };
 
+      # this includes clang, etc
+      lxStdEnv = pkgs.swiftPackages.stdenv;
+
       lxPackages = with pkgs; [
           # system tools
           zsh
           git
           curl
 
-          # Low-level (Objective) C/++ toolchain
-          gcc14
-          clang-tools
-          # High-level swift toolchain
-          # TODO add swift here once the nix version is updated to 6, for now, we have to use debian
-          # swift
-          #swiftPackages.Foundation
-
           # to run the web app
           nodejs_23
-
-          # toolchain needed for webassembly
-          emscripten
-
-          # build tools
-          cmake
-          ninja
-          perl
 
           # for building the documentation
           texliveFull
           
           # for the web part
           nodejs_22
+
+          # build tools
+          cmake
+          ninja
+          swift
       ];
+
+      environment = {};
     in {
       devShells = {
         default = pkgs.mkShell.override
           {
-            stdenv = pkgs.clangStdenv;
+            stdenv = lxStdEnv;
           }
           {
             packages = with pkgs; lxPackages ++ [
@@ -95,38 +82,24 @@
               # useful for development
               swi-prolog
             ];
-
-            shellHook = ''
-                echo "Since nix does not have a recent swift version, we need to include it via apt"
-                sudo apt install -y swiftlang
-                export DONT_PROMPT_WSL_INSTALL=y
-            '';
           };
+
+          env = environment;
       };
 
       packages = {
-        default = 
-        let
-          fs = pkgs.lib.fileset;
-          sourceFiles = fs.unions [
-            ./CMakeLists.txt
-            ./Sources
-            ./Dependencies
-          ];
-        in
-        pkgs.clangStdenv.mkDerivation {
+        default =  lxStdEnv.mkDerivation {
           name = "lx";
-          src = fs.toSource {
-            root = ./.;
-            fileset = sourceFiles;
-            };
+          src = ./.;
           
           buildInputs = lxPackages; # TODO: convert to nativeBuildInputs, make swift static
 
           cmakeFlags = [
-            "-GNinja"
+            "-G Ninja"
             "--preset release-x86-64-unknown-linux-gnu"
           ];
+
+          env = environment;
         };
       };
     }

548b3b2d228b40e3f10016c9894a068a4d15cabd
freiseisen <lukas.freiseisen@scch.at>
2025-02-10 16:47:04 +0100

    removed usage of keycloak service

--- Sources/lxui/src/main.tsx
+++ Sources/lxui/src/main.tsx
@@ -7,16 +7,16 @@ import { KeycloakService } from './services/keycloak/KeycloakService';
 import { ReactKeycloakProvider } from '@react-keycloak/web';
 import defaultKeycloak from './config/KeycloakConfig';
 
-const keycloakService = new KeycloakService();
+//const keycloakService = new KeycloakService();
 /*
  * TODO current design decision goes against reactive thinking.
  * => change to KeyCloakProvider wrapping PrologVMProvider wrapping App
  * https://www.geeksforgeeks.org/how-to-implement-keycloak-authentication-in-react/#adding-your-react-app-to-keycloak
  */
-keycloakService
-  .isAuthenticated()
-  .then((isAuthenticated) => {
-    if (isAuthenticated) {
+//keycloakService
+//  .isAuthenticated()
+//  .then((isAuthenticated) => {
+//    if (isAuthenticated) {
       ReactDOM.createRoot(document.getElementById('root')!).render(
         <ReactKeycloakProvider
           authClient={defaultKeycloak}
@@ -25,8 +25,8 @@ keycloakService
           <App />
         </ReactKeycloakProvider>
       );
-    } else {
-      console.log('Not authenticated');
-    }
-  })
-  .catch((err) => console.log(err));
+//    } else {
+//      console.log('Not authenticated');
+//    }
+//  })
+//  .catch((err) => console.log(err));

33b561d59d189f4f1658a322aa63fca4fb0cfd83
Alex <kiwi-uncurled-salt@duck.com>
2025-03-14 15:43:28 +0100

    moved Dockerfile

--- Playground/main.swift
+++
@@ -1,7 +0,0 @@
-// swiftc main.swift ../generated/SwiftBridgeCore.swift ../generated/LX-rs/LX-rs.swift -import-objc-header ../bridging-header.h -L../target/debug -lcorpus
-
-import Foundation
-
-let content: RustString = fetch_from_corpus("labgg.pl")
-let contentStr = content.toString()
-print(" \(contentStr))")

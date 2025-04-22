8607cf3334f3f474657fbdb76c0133e6c73bb6f5
Alex <kiwi-uncurled-salt@duck.com>
2025-04-22 11:07:11 +0000

    feat: add tokio dependency and enhance PrologVM with async module loading and execution

--- Sources/LogicKit/src/lib.rs
+++ Sources/LogicKit/src/lib.rs
@@ -1,19 +1,48 @@
-#![allow(non_snake_case)]
-
 pub struct PrologVM {
     js_runtime: deno_core::JsRuntime,
 }
 
 impl PrologVM {
-    pub fn new() -> Self {
-        let js_runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
-            module_loader: Some(std::rc::Rc::new(deno_core::FsModuleLoader)),
-            ..Default::default()
-        });
+    pub fn from_options(options: deno_core::RuntimeOptions) -> Self {
+        let js_runtime = deno_core::JsRuntime::new(options);
 
         return Self {
             js_runtime: js_runtime,
         };
+    }
+
+    pub fn new() -> Self {
+        let options = deno_core::RuntimeOptions {
+            module_loader: Some(std::rc::Rc::new(deno_core::FsModuleLoader)),
+            ..Default::default()
+        };
+        return Self::from_options(options);
+    }
+
+    pub async fn load_module_from_file(
+        &mut self,
+        file_path: &str,
+    ) -> Result<deno_core::ModuleId, deno_core::error::CoreError> {
+        let main_module = deno_core::resolve_path(file_path, &std::env::current_dir()?)
+            .expect("Error converting path to URL");
+        let mod_id = self.js_runtime.load_main_es_module(&main_module).await?;
+        return Ok(mod_id);
+    }
+
+    pub async fn evaluate_module(
+        &mut self,
+        mod_id: deno_core::ModuleId,
+    ) -> Result<impl ?Sized, deno_core::error::CoreError> {
+        let result = self.js_runtime.mod_evaluate(mod_id);
+        self.js_runtime.run_event_loop(Default::default()).await?;
+        return Ok(result);
+    }
+
+    pub async fn execute(
+        &mut self,
+        script: String,
+    ) -> Result<deno_core::v8::Global<deno_core::v8::Value>, deno_core::error::CoreError> {
+        self.js_runtime.execute_script("script.js", script)
     }
 }
 
@@ -21,6 +50,9 @@ impl PrologVM {
 mod ffi {
     extern "Rust" {
         type PrologVM;
+
+        #[swift_bridge(init)]
+        fn new() -> PrologVM;
     }
 }
 
@@ -28,9 +60,15 @@ mod ffi {
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn test_execute_script() -> Result<(), deno_core::error::AnyError> {
+        let mut vm = PrologVM::new();
+        let script = "let x = 2 + 2; x;".to_string();
+        let result = vm.execute(script).await?;
+        let scope = &mut vm.js_runtime.handle_scope();
+        let value = result.open(scope);
+        let value = value.to_integer(scope).unwrap().value();
+        assert_eq!(value, 4);
+        Ok(())
     }
 }
